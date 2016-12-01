25e7cacccfd6d068b831887fcd10b7c6bcf457f3
mstoth <michael@virtualpianist.com>
2016-11-30 23:08:36 -0500

    added instructions

--- Quiz3/functional_tests.py
+++ Quiz3/functional_tests.py
@@ -26,6 +26,14 @@ class NewVisitorTest(unittest.TestCase):
         """
 
         Quiz 3. Add A Page for me.
+	
+	To this web site, do the following: 
+
+	1. Add a test for a title on the home page and then add the title
+	2. Add a test for an image of your choice and then insert the image into the home page. 
+	3. Add a test for a clickable area on the image then add the clickable image area
+	4. Add a test for the result of clicking on the image area (the new page) then add the web page to make it pass
+	5. Test for a header on the new page and then add the header. 
 
         """
 
